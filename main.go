1bc18b16ab2475c75b414b60d200240267455f02
hisayuki_mori <h.mori0206@gmail.com>
2022-08-03 22:06:28 +0900

    おためしプログラム

--- main.go
+++ main.go
@@ -3,25 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"strconv"
 )
 
 func main() {
-  var help = false
-  var format = ""
-  flag.BoolVar(&help, "h", false, "show help")
-  flag.StringVar(&format,"f","%s\n", "set format")
-  flag.Parse()
+	var help = false
+	var format = ""
+	flag.BoolVar(&help, "h", false, "show help")
+	flag.StringVar(&format,"f", "%s\n", "set format")
+	flag.Parse()
 
-  if help {
-    flag.Usage()
-    return
-  }
+	if help {
+		flag.Usage()
+		return
+	}
 
-  for i, arg := range flag.Args() {
-    fmt.Print(strconv.FormatInt(int64(i), 10) + ":")
-    fmt.Print(strconv.Itoa(i) + ":")
-    fmt.Print("%d", i)
-    fmt.Print(format, arg)
-  }
+    dict := map[string][]string {
+		"kanji": {"零", "壱", "弐", "参", "肆"},
+		"roma": {"?", "Ⅰ", "Ⅱ", "Ⅲ", "Ⅳ"},
+	}
+
+
+	for _, arg := range flag.Args() {
+		n, err := strconv.Atoi(arg)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "数値のパースに失敗しました:" +err.Error())
+			os.Exit(1)
+		}
+		if n < 0 || n > 4 {
+			fmt.Fprintln(os.Stderr, "数値が範囲外です。0~4にしてください: " +arg)
+			os.Exit(1)
+		}
+		if dict[format] == nil {
+			fmt.Fprintln(os.Stderr, "kanji か roma を指定してね" +format)
+			os.Exit(1)
+		}
+		fmt.Println(dict[format][n])
+	}
 }
