cef675e7731974a745e7e7a683951f7fdd842a06
hisayuki_mori <h.mori0206@gmail.com>
2024-05-28 00:49:45 +0900

    fix: swg format

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -6,10 +6,10 @@ import (
 	"github.com/mori-hisayuki/go-study/api/internal/presenter"
 )
 
-// @title ユーザー管理サービスAPI
-// @version 0.1.0
-// @description ユーザー管理サービスAPIの昨日
-// @host localhost:8080
+// @title			ユーザー管理サービスAPI
+// @version		0.1.0
+// @description	ユーザー管理サービスAPIの昨日
+// @host			localhost:8080
 func main() {
 	server := presenter.NewServer()
 	if err := server.Run(context.Background()); err != nil {
